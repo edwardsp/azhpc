9d7fb2b5cc10b0dc7d03cb1895363b1612377cbd
paul <Paul.Edwards@Microsoft.com>
2017-07-18 10:00:18 +0000

    Fixed errors in script

--- benchmarks/run_ANSYS_Fluent_Sedan4m_benchmark.sh
+++ benchmarks/run_ANSYS_Fluent_Sedan4m_benchmark.sh
@@ -2,12 +2,15 @@
 
 if [[ $# -lt 1 ]]; then
 	echo "Usage: $0 <BENCHMARK>"
-	echo "Benchmarks available: sedan_4m.tar, f1_racecar_140m"
+	echo "Benchmarks available: sedan_4m, f1_racecar_140m"
+	exit 1
 fi
 
 BENCHMARK=$1
 
 echo "Benchmark: $BENCHMARK"
+
+wget http://azbenchmarkstorage.blob.core.windows.net/ansysbenchmarkstorage/${BENCHMARK}.tar -O - | tar x || exit 1
 
 cat <<EOF >run_${BENCHMARK}.jou
 f r-c-d ${BENCHMARK}.cas.gz
@@ -18,7 +21,5 @@ parallel timer usage
 exit y
 EOF
 
-wget http://azbenchmarkstorage.blob.core.windows.net/ansysbenchmarkstorage/${BENCHMARK}.tar -O - | tar x
-
 echo "Complete - use the following command to run:"
-echo "    fluent 3d -g -mpi=intel -pib.dapl -ssh -t256 -cnf=hosts -i ${BENCHMARK.jou"
+echo "    fluent 3d -g -mpi=intel -pib.dapl -ssh -t256 -cnf=hosts -i ${BENCHMARK}.jou"
