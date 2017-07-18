40629468bb809e85671661a5fdcbe6612532ef99
paul <Paul.Edwards@Microsoft.com>
2017-07-18 15:25:01 +0000

    Removed ansys subdir in the path

--- install/install_ANSYS_Fluent.sh
+++ install/install_ANSYS_Fluent.sh
@@ -6,27 +6,27 @@ echo $USER,$LICIP,$HOST
 
 sudo yum groupinstall -y "X Window System"
 
-mkdir /mnt/resource/scratch/ansys
-mkdir /mnt/resource/scratch/ansys/INSTALLERS
+mkdir /mnt/resource/scratch
+mkdir /mnt/resource/scratch/INSTALLERS
 
-cd /mnt/resource/scratch/ansys/INSTALLERS
+cd /mnt/resource/scratch/INSTALLERS
 wget -q http://azbenchmarkstorage.blob.core.windows.net/ansysbenchmarkstorage/ANSYS.tgz -O - | tar zx
 
-cd /mnt/resource/scratch/ansys/INSTALLERS/ANSYS/
-mkdir -p /mnt/resource/scratch/ansys/applications/ansys_inc/shared_files/licensing/
+cd /mnt/resource/scratch/INSTALLERS/ANSYS/
+mkdir -p /mnt/resource/scratch/applications/ansys_inc/shared_files/licensing/
 
-cat <<EOF > /mnt/resource/scratch/ansys/applications/ansys_inc/shared_files/licensing/ansyslmd.ini
+cat <<EOF > /mnt/resource/scratch/applications/ansys_inc/shared_files/licensing/ansyslmd.ini
 SERVER=1055@$LICIP
 ANSYSLI_SERVERS=2325@$LICIP
 EOF
 
 cat <<EOF >> $HOST/.bashrc
 export FLUENT_HOSTNAME=$HOST
-export PATH=/mnt/resource/scratch/ansys/applications/ansys_inc/v172/fluent/bin:\$PATH
+export PATH=/mnt/resource/scratch/applications/ansys_inc/v172/fluent/bin:\$PATH
 EOF
 
-source /mnt/resource/scratch/ansys/INSTALLERS/ANSYS/INSTALL -silent -install_dir "/mnt/resource/scratch/ansys/applications/ansys_inc/" -fluent
-#source /mnt/resource/scratch/ansys/INSTALLERS/ANSYS/INSTALL -silent -install_dir "/mnt/resource/scratch/ansys/applications/ansys_inc/" -cfx
+source /mnt/resource/scratch/ansys/INSTALLERS/ANSYS/INSTALL -silent -install_dir "/mnt/resource/scratch/applications/ansys_inc/" -fluent
+#source /mnt/resource/scratch/ansys/INSTALLERS/ANSYS/INSTALL -silent -install_dir "/mnt/resource/scratch/applications/ansys_inc/" -cfx
 
 
 
