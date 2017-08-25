9f791d7b5b554e1c692b8c2b5c62b6f397384d63
Paul Edwards <paedwar@microsoft.com>
2017-08-25 14:01:21 +0100

    Added installed for starccm 12.02

---
+++ install/install_StarCCM_1202.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+LICIP=$1
+HOST=`hostname`
+echo $LICIP,$HOST
+
+sudo yum groupinstall -y "X Window System"
+
+mkdir -p /mnt/resource/scratch/applications
+mkdir -p /mnt/resource/scratch/INSTALLERS
+
+cd /mnt/resource/scratch/INSTALLERS
+wget -q http://azbenchmarkstorage.blob.core.windows.net/cdadapcobenchmarkstorage/STAR-CCM+12.02.010_01_linux-x86_64.tar.gz -O - | tar zx
+
+cd /mnt/resource/scratch/INSTALLERS/starccm+_12.02.010
+
+cat <<EOF >> $HOME/.bashrc
+export PODKey=$LICIP
+export CDLMD_LICENSE_FILE=1999@flex.cd-adapco.com
+export PATH=/mnt/resource/scratch/applications/12.02.010/STAR-CCM+12.02.010/star/bin:/opt/intel/compilers_and_libraries_2017.2.174/linux/mpi/intel64/bin:\$PATH
+EOF
+
+sh /mnt/resource/scratch/INSTALLERS/starccm+_12.02.010/STAR-CCM+12.02.010_01_linux-x86_64-2.5_gnu4.8.bin -i silent -DINSTALLDIR=/mnt/resource/scratch/applications -DNODOC=true -DINSTALLFLEX=false
