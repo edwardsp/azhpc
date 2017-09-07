76a150a6d76e903dbabd8a47795a22cd0d5db1f0
Paul Edwards <paedwar@microsoft.com>
2017-09-07 18:38:10 +0100

    Minor restructure and added starccm benchmark

--- automation/deploy.sh
+++ automation/deploy.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+function required_envvars {
+        condition_met=true
+        for i in "$@"; do
+        if [ -z "$i" ]; then
+                        echo "ERROR: $i needs to be set."
+                        condition_met=false
+                else
+                        echo "$i=${!i}"
+                fi
+        done
+        if [ "$condition_met" = "false" ]; then
+                echo
+                exit 1
+        fi
+}
+
 echo "Reading parameters from: $1"
 source $1
+required_envvars resource_group vmSku vmssName computeNodeImage instanceCount rsaPublicKey
+
+benchmarkScript=$2
+echo "Benchmark script: $benchmarkScript"
+source $benchmarkScript
 
 scriptname=$(basename "$0")
 scriptname="${scriptname%.*}"
@@ -10,16 +31,16 @@ LOGDIR=${scriptname}_${timestamp}
 mkdir $LOGDIR
 
 function execute {
-	task=$1
-	SECONDS=0
-	echo -n "Executing: $2"
-	for a in "${@:3}"; do
-		echo -n " '$(echo -n $a | tr '\n' ' ')'"
-	done
-	echo
-	$2 "${@:3}" 2>&1 | tee $LOGDIR/${task}.log
-	duration=$SECONDS
-	echo "$task $duration" | tee -a $LOGDIR/times.log
+        task=$1
+        SECONDS=0
+        echo -n "Executing: $2"
+        for a in "${@:3}"; do
+                echo -n " '$(echo -n $a | tr '\n' ' ')'"
+        done
+        echo
+        $2 "${@:3}" 2>&1 | tee $LOGDIR/${task}.log
+        duration=$SECONDS
+        echo "$task $duration" | tee -a $LOGDIR/times.log
 }
 
 # assuming already logged in a the moment
@@ -28,8 +49,8 @@ function execute {
 
 # make sure the resource group does not exist
 if [ "$(az group exists --name $resource_group)" = "true" ]; then
-	echo "Error: Resource group already exists"
-	exit 1
+        echo "Error: Resource group already exists"
+        exit 1
 fi
 
 # create the resource group
@@ -37,21 +58,21 @@ execute "create_resource_group" az group create --name "$resource_group" --locat
 
 parameters=$(cat << EOF
 {
-	"vmSku": {
-		"value": "$vmSku"
-	},
-	"vmssName": {
-		"value": "$vmssName"
-	},
-	"computeNodeImage": {
-		"value": "$computeNodeImage"
-	},
-	"instanceCount": {
-		"value": $instanceCount
-	},
-	"rsaPublicKey": {
-		"value": "$rsaPublicKey"
-	}
+        "vmSku": {
+                "value": "$vmSku"
+        },
+        "vmssName": {
+                "value": "$vmssName"
+        },
+        "computeNodeImage": {
+                "value": "$computeNodeImage"
+        },
+        "instanceCount": {
+                "value": $instanceCount
+        },
+        "rsaPublicKey": {
+                "value": "$rsaPublicKey"
+        }
 }
 EOF
 )
@@ -63,8 +84,11 @@ execute "deploy_azhpc" az group deployment create \
     --parameters "$parameters"
 
 public_ip=$(az network public-ip list --resource-group "$resource_group" --query [0].dnsSettings.fqdn | sed 's/"//g')
+
 execute "get_hosts" ssh hpcuser@${public_ip} nmapForHosts
 execute "show_bad_nodes" ssh hpcuser@${public_ip} testForBadNodes
 
-execute "delete_resource_group" az group delete --name "$resource_group" --yes
+# run the benchmark function
+run_benchmark
 
+execute "delete_resource_group" az group delete --name "$resource_group" --yes
--- automation/parameters.tpl
+++ automation/parameters.tpl
@@ -1,7 +1,12 @@
-resource_group=<INSERT-RESOURCE-GROUP>
-location="North Central US"
+my_uid=$(uuidgen | cut -c1-6)
+
+resource_group=azhpc-${my_uid}
 vmSku=Standard_H16r
-vmssName=<INSERT-VMSS-NAME>
+vmssName=az${my_uid}
 computeNodeImage=CentOS-HPC_7.1
-instanceCount=4
-rsaPublicKey=<INSERT-RSA-PUBLIC-KEY>
+instanceCount=16
+rsaPublicKey=$(cat ~/.ssh/id_rsa.pub)
+
+numberOfNodesToTest=(8 16)
+processesPerNode=16
+podKey=
