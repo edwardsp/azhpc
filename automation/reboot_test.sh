a259956fcec2ac32c10c3b32e7c5deb775687c65
Paul Edwards <paedwar@microsoft.com>
2017-09-08 14:48:36 +0100

    Added more checks for the reboot test.

--- automation/reboot_test.sh
+++ automation/reboot_test.sh
@@ -1,5 +1,7 @@
 
 function run_benchmark() {
+    rebootLogFile=$(get_log "reboot_failures")
+    touch $rebootLogFile
     for reboot in $(seq 1 10); do
         execute "reboot_${reboot}" az vmss restart --name "$vmssName" --resource-group "$resource_group"
         
@@ -14,10 +16,23 @@ function run_benchmark() {
         done
 
         if [ "$working_hosts" -ne "$instanceCount" ]; then
-            echo "Error: all hosts are not accessible with ssh."
-            clear_up
+            echo "Reboot $reboot failed: All hosts are not accessible with ssh" | tee -a $rebootLogFile
+            continue
         fi
 
         execute "reboot_${reboot}_show_bad_nodes" ssh hpcuser@${public_ip} testForBadNodes
+        retry=1
+        bad_nodes=$(grep fail $(get_log "reboot_${reboot}_show_bad_nodes") | wc -l)
+        while [ "$retry" -lt "6" -a "$bad_nodes" -ne "0" ]; do
+            sleep 60
+            execute "reboot_${reboot}_show_bad_nodes_retry_$retry" ssh hpcuser@${public_ip} testForBadNodes
+            bad_nodes=$(grep fail $(get_log "reboot_${reboot}_show_bad_nodes_retry_$retry") | wc -l)
+            let retry=$retry+1
+        done
+
+        if [ "$bad_nodes" -ne "0" ]; then
+            echo "Reboot $reboot failed: $bad_nodes bad nodes." | tee -a $rebootLogFile
+            continue
+        fi
     done
 }
