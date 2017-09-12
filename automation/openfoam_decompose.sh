c3ae107353060303d947361e11b2436704441276
Paul Edwards <paedwar@microsoft.com>
2017-09-12 18:32:21 +0100

    Fixing bugs
    - incorrect function name
    - not ssh-ing to a node

--- automation/openfoam_decompose.sh
+++ automation/openfoam_decompose.sh
@@ -1,13 +1,13 @@
 required_envvars nnodes_list ppn_list storageAccountName storageContainerName storageBenchmarkPath storageBenchmarkName storageSasKey
 
-function benchmark {
+function run_benchmark {
 
     execute "install_openfoam" ssh hpcuser@${public_ip} azhpc/install/install_openfoam.sh
     execute "download_case" ssh hpcuser@${public_id} "cd /mnt/resource/scratch && wget https://$storageAccountName.blob.core.windows.net/$storageContainerName/$storageBenchmarkPath/$storageBenchmarkName.tar -O - | tar xvf"
 
     for NNODES in nnodes_list; do
         for PPN in ppn_list; do
-            execute "create_case_${NNODES}x${PPN}" ssh hpcuser@${public_ip} "cd /mnt/resource/scratch && ./$storageBenchmarkName/create_case.sh $NNODES $PPN"
+            execute "create_case_${NNODES}x${PPN}" ssh hpcuser@${public_ip} "ssh \$(cat bin/hostlist) 'cd /mnt/resource/scratch && ./$storageBenchmarkName/create_case.sh $NNODES $PPN'"
             decomposedCase=${storageBenchmarkName}_${NNODES}x${PPN}
             execute "copy_$decomposedCase" scp hpcuser@${public_ip}:/mnt/resource/scratch/$decomposedCase.tar .
             execute "upload_$decomposedCase" az storage blob upload \
