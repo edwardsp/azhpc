5a70c9436d7d1c798a468e5704c8ea6086bc27a6
Xavier Pillons <xpillons@microsoft.com>
2017-09-14 19:17:31 +0200

    fix host / sshin test

--- automation/deploy.sh
+++ automation/deploy.sh
@@ -79,7 +79,7 @@ public_ip=$(az network public-ip list --resource-group "$resource_group" --query
 execute "get_hosts" ssh hpcuser@${public_ip} nmapForHosts
 working_hosts=$(sed -n "s/.*sshin=\([^;]*\).*/\1/p" $(get_log "get_hosts"))
 retry=1
-while [ "$retry" -lt "6" -a "$working_hosts" -eq "$instanceCount" ]; do
+while [ "$retry" -lt "6" -a "$working_hosts" -ne "$instanceCount" ]; do
         sleep 60
         execute "get_hosts_retry_$retry" ssh hpcuser@${public_ip} nmapForHosts
         working_hosts=$(sed -n "s/.*sshin=\([^;]*\).*/\1/p" $(get_log "get_hosts_retry_$retry"))
@@ -88,7 +88,7 @@ done
 
 telemetryData="$(jq ".clusterDeployment.sshretries=\"$retry\"" <<< $telemetryData)"
 
-if [ "$working_hosts" -eq "$instanceCount" ]; then
+if [ "$working_hosts" -ne "$instanceCount" ]; then
         echo "Error: all hosts are not accessible with ssh."
         telemetryData="$(jq ".clusterDeployment.status=\"failed\"" <<< $telemetryData)"
         clear_up
