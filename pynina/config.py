958863caa87ee8465395b7911cff92adec2cc9c7
Xavier Pillons <xpillons@microsoft.com>
2017-09-18 13:44:27 +0200

    removed pynina

--- pynina/config.py
+++
@@ -1,5 +0,0 @@
-""" Nina configuration """
-DOCUMENTDB_DATABASE = 'Nina'
-DOCUMENTDB_COLLECTION = 'Results'
-DOCUMENTDB_ENDPOINT = 'https://ninadb.documents.azure.com:443/'
-DOCUMENTDB_AUTHKEY = 'tH0qjX7LoEDNlGH8gER9j7wqXejslxoqwxTguV1iQi7vWsiQV5hooafnQvnIJRsXWrJEixrZ2RBiEP59kBt1Xg=='
