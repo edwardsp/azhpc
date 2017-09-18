5d61ce86e2b6c6bc4d4e4c444fcc61843ac763b2
Xavier Pillons <xpillons@microsoft.com>
2017-09-18 12:30:37 +0200

    reading parameters from appsettings.json

--- corenina/Program.cs
+++ corenina/Program.cs
@@ -7,16 +7,18 @@ using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
 
 namespace corenina
 {
     class Program
     {
-        private const string _endpointUri = "https://ninadb.documents.azure.com:443/";
-        private const string _primaryKey = "tH0qjX7LoEDNlGH8gER9j7wqXejslxoqwxTguV1iQi7vWsiQV5hooafnQvnIJRsXWrJEixrZ2RBiEP59kBt1Xg==";
+        // private const string _endpointUri = "https://ninadb.documents.azure.com:443/";
+        // private const string _primaryKey = "tH0qjX7LoEDNlGH8gER9j7wqXejslxoqwxTguV1iQi7vWsiQV5hooafnQvnIJRsXWrJEixrZ2RBiEP59kBt1Xg==";
         private const string _dbName = "Nina";
         private const string _collectionId = "Results";
-        private DocumentClient client;
+        private DocumentClient _client;
+        private static IConfigurationRoot _config;
 
         static void Main(string[] args)
         {
@@ -27,6 +29,9 @@ namespace corenina
                     Console.WriteLine("Missing json file path");
                     return;
                 }
+                // Get the configuration
+                _config = BuildConfiguration();
+                
                 Program p = new Program();
                 p.UploadJsonFile(args[0]).Wait();
             }
@@ -48,12 +53,25 @@ namespace corenina
             {
                 string json = sr.ReadToEnd();
                 JObject o = JObject.Parse(json);
-                this.client = new DocumentClient(new Uri(_endpointUri), _primaryKey);
+                this._client = new DocumentClient(new Uri(_config["EndpointUri"]), _config["PrimaryKey"]);
 
-                ResourceResponse<Document> response = await this.client.UpsertDocumentAsync(UriFactory.CreateDocumentCollectionUri(_dbName, _collectionId), o);
+                ResourceResponse<Document> response = await this._client.UpsertDocumentAsync(UriFactory.CreateDocumentCollectionUri(_dbName, _collectionId), o);
                 var createdDocument = response.Resource;
                 Console.WriteLine("Document with id {0} created", createdDocument.Id);
             }
         }
+
+        /// <summary>
+        /// Build the confguration
+        /// </summary>
+        /// <returns>Returns the configuration</returns>
+        private static IConfigurationRoot BuildConfiguration()
+        {
+            // Enable to app to read json setting files
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
+
+            return builder.Build();
+        }        
     }
 }
