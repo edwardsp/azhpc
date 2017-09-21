7c12c7f0c32171b82990d0524663ed05d3f47fff
Xavier Pillons <xpillons@microsoft.com>
2017-09-21 19:28:35 +0200

    added HPL params

--- automation/singleNodeHPL.sh
+++ automation/singleNodeHPL.sh
@@ -1,17 +1,23 @@
-#
-# TODO : add N, P, Q, NB parameters based on the VM size
-#
+required_envvars vmSku
 
 function run_benchmark() {
 	execute "get_hpl" ssh hpcuser@${public_ip} "wget 'https://pintaprod.blob.core.windows.net/private/hpl.tgz?sv=2016-05-31&si=read&sr=b&sig=5ZluFkKL%2F3GyNexDVQBB1sEmUdHpkutLlXaLfE%2BmUN4%3D' -q -O -  | tar zx --skip-old-files"
 	
-	# run HPL. problem size 69120 is allocating 36 GB. For 64GB use 92160.
+	hplParams=$(jq '.singleNode[] | select(.vmSize == $data)' --arg data $vmSku hplparams.json)
+	N=$(jq '.N' <<< $hplParams)
+	NB=$(jq '.NB' <<< $hplParams)
+	P=$(jq '.P' <<< $hplParams)
+	Q=$(jq '.Q' <<< $hplParams)
+	
+	# run HPL
 	# 
     for i in $(seq 1 5); do	
-		execute "run_hpl${i}" ssh hpcuser@${public_ip} "pdsh 'cd hpl; mpirun -np 2 -perhost 2 ./xhpl_intel64_static -n 69120 -p 1 -q 2 -nb 192 | grep WC00C2R2'"
+		execute "run_hpl${i}" ssh hpcuser@${public_ip} "pdsh 'cd hpl; mpirun -np 2 -perhost 2 ./xhpl_intel64_static -n $N -p $P -q $Q -nb $NB | grep WC00C2R2'"
 	done
 
-	jsonRoot="$(jq -n '.singlehpl.parameters.N=69120 | .singlehpl.parameters.P=1 | .singlehpl.parameters.Q=2 | .singlehpl.parameters.NB=192 | .singlehpl.results=[]' )"
+	jsonRoot="$(jq -n '.singlehpl=$data | .singlehpl.results=[]' --argjson data "$hplParams")"
+
+	#jsonRoot="$(jq -n '.singlehpl.parameters.N=69120 | .singlehpl.parameters.P=1 | .singlehpl.parameters.Q=2 | .singlehpl.parameters.NB=192 | .singlehpl.results=[]' )"
 	json="$(cat $LOGDIR/run_hpl*.log | jq -s -R 'split("\n") | map(select(contains("WC00C2R2"))) | map(split(" ") | map(select(. != ""))) | map({"hostname": .[0]|rtrimstr(":"),"duration": .[6],"gflops": .[7]})')"
 	jsonBenchmark="$(jq '.singlehpl.results=$data' --argjson data "$json" <<< $jsonRoot)"
 }
