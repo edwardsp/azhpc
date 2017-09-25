dc6c54c7b3eeba2fbf97b1fef0c1e1b26926620c
Paul Edwards <paedwar@microsoft.com>
2017-09-25 23:03:07 +0800

    fixed loop

--- benchmarks/run_ring_pingpong.sh
+++ benchmarks/run_ring_pingpong.sh
@@ -3,7 +3,7 @@
 hostlist=$HOME/bin/hostlist
 
 src=$(tail -n1 $hostlist)
-cat $hostlist | while read line; do
+for line in $(<$hostlist); do
     dst=$line
     mpirun -np 2 -ppn 1 -hosts $src,$dst IMB-MPI1 PingPong | tee ${src}_to_${dst}_pingpong.log
     src=$dst
