eaa7ec134d2f02f4d5a9d90bfcea0db0283762c1
Xavier Pillons <xpillons@microsoft.com>
2017-09-26 17:27:43 +0800

    update packages for axel and Star

--- install/install_StarCCM_1202.sh
+++ install/install_StarCCM_1202.sh
@@ -9,8 +9,8 @@ mkdir -p /mnt/resource/scratch/applications
 mkdir -p /mnt/resource/scratch/INSTALLERS
 
 cd /mnt/resource/scratch/INSTALLERS
-sudo yum install -y epel*
-sudo yum install -y axel libXext
+
+sudo yum install -y libXext libXt
 axel -q -n 50 http://azbenchmarkstorage.blob.core.windows.net/cdadapcobenchmarkstorage/STAR-CCM+12.02.010_01_linux-x86_64.tar.gz
 tar xzf STAR-CCM+12.02.010_01_linux-x86_64.tar.gz
 
