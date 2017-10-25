b7d774a5ec48ee48e71f6083dfe253023659845a
Paul Edwards <paedwar@microsoft.com>
2017-10-25 15:57:02 +0100

    bash error

--- automation/hpl_benchmark.sh
+++ automation/hpl_benchmark.sh
@@ -12,7 +12,7 @@ function run_benchmark() {
     # taken from here: http://www.crc.nd.edu/~rich/CRC_Summer_Scholars_2014/HPL-HowTo.pdf
     fullLinpack_N=$(bc <<< "((sqrt(${fullLinpack_MemoryPerNode}*${instanceCount}*1024^3/8))/${fullLinpack_NB})*${fullLinpack_NB}")
 
-    execute "run_full_linpack" ssh hpcuser@${public_ip} "ssh \$(head -n bin/hostlist) 'cd hpl && mpirun -np $mpiRanks -ppn 2 -hostfile \$HOME/bin/hostlist ./xhpl_intel64_static -n $fullLinpack_N -p $fullLinpack_P -q $fullLinpack_Q -nb $fullLinpack_NB'"
+    execute "run_full_linpack" ssh hpcuser@${public_ip} "ssh \$(head -n1 bin/hostlist) 'cd hpl && mpirun -np $mpiRanks -ppn 2 -hostfile \$HOME/bin/hostlist ./xhpl_intel64_static -n $fullLinpack_N -p $fullLinpack_P -q $fullLinpack_Q -nb $fullLinpack_NB'"
     
     benchmarkData="{}"
 
