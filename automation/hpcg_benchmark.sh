c0797a45e3b92b1ebd4de64af19f27df7fe5c784
Paul Edwards <paedwar@microsoft.com>
2017-10-27 18:40:15 +0100

    HPCG benchmark added

---
+++ automation/hpcg_benchmark.sh
@@ -0,0 +1,15 @@
+required processesPerNode
+
+function run_benchmark() {
+    execute "run_hpcg_benchmark" ssh hpcuser@${public_ip} "wget -q 'https://paedwar.blob.core.windows.net/public/hpcg-bin.tgz' -O - | tar zx"
+    
+    numProcs=$(bc <<< "$instanceCount * $processesPerNode")
+
+    execute "run_hpcg_benchmark" ssh hpcuser@${public_ip} "ssh \$(head -n1 bin/hostlist) 'cd hpcg-bin && mpirun -np $numProcs -ppn $processesPerNode -hostfile \$HOME/bin/hostlist ./xhpcg'"
+
+    execute "get_hpcg_results" ssh hpcuser@${public_ip} "cat hpcg-bin/HPCG-Benchmark_*"
+
+    HPCG_GFLOPs=$(grep "rating of" $(get_log get_hpcg_results) | cut -d'=' -f2)
+
+    benchmarkData="{ \"GFLOPs\" : \"$HPCG_GFLOPs\" }"
+}
