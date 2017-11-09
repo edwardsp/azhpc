23c93b41cc22a8218b2ed5be7174ee30edb58e88
Paul Edwards <paedwar@microsoft.com>
2017-11-08 21:03:07 -0800

    Escaping env vars

--- install/install_paraview.sh
+++ install/install_paraview.sh
@@ -7,8 +7,8 @@ sudo yum install -y mesa-libGLU libSM libXt
 pdsh 'sudo yum install -y mesa-libGLU libSM libXt'
 
 cat <<EOF >> $HOME/.bashrc
-export LD_LIBRARY_PATH=$HOME/ParaView-5.4.1-Qt5-OpenGL2-MPI-Linux-64bit/lib:$LD_LIBRARY_PATH
-export PATH=$HOME/ParaView-5.4.1-Qt5-OpenGL2-MPI-Linux-64bit/bin:$PATH
+export LD_LIBRARY_PATH=\$HOME/ParaView-5.4.1-Qt5-OpenGL2-MPI-Linux-64bit/lib:\$LD_LIBRARY_PATH
+export PATH=\$HOME/ParaView-5.4.1-Qt5-OpenGL2-MPI-Linux-64bit/bin:\$PATH
 EOF
 
 
