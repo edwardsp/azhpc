d4a670ee337c65932e151ac614f300ec41bb7a21
Paul Edwards <paedwar@microsoft.com>
2017-11-08 18:32:36 -0800

    Added surfboard run

---
+++ automation/surfboard_run.sh
@@ -0,0 +1,13 @@
+required_envvars processesPerNode numberOfIterations surfboardUrl roll depth
+
+function run_benchmark() {
+    execute "install_openfoam" ssh hpcuser@${public_ip} "./azhpc/install/install_openfoam.sh"
+    execute "install_paraview" ssh hpcuser@${public_ip} "./azhpc/install/install_paraview.sh"
+    execute "download_surfboard_case" ssh hpcuser@${public_ip} "git clone https://github.com/edwardsp/surfboard.git"
+    numProcs=$(bc <<< "$instanceCount * $processesPerNode")
+    mpiArgs="-np $numProcs -ppn $processesPerNode -hostfile \$HOME/bin/hostlist"
+    execute "run_surfboard_case" ssh hpcuser@${public_ip} "ssh \$(head -n1 bin/hostlist) 'cd surfboard && ./run.sh \"$surfboardUrl\" $depth $roll $numProcs $numberOfIterations \"$mpiArgs\"'"
+    rsync -az hpcuser@${public_ip}:surfboard $LOGDIR/.
+
+    benchmarkData="{}"
+}
